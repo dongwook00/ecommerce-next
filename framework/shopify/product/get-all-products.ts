5aba1ba3145793a8be7d55ce0796ef8a9f6de040
Dongwook <dongwook227@gmail.com>
2022-03-21 16:31:49 -0400

    fetch product query

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,10 +1,19 @@
-const fetchApi = async () => {
-  const url = "https://jsonplaceholder.typicode.com/todos"
+import getAllProductQuery from "../utils/queries/get-all-products"
+
+type FetchParams = {
+  query: string
+}
+
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = "http://localhost:4000/graphql"
   const res = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json"
-    }
+    },
+    body: JSON.stringify({
+      query
+    })
   })
 
   const data = await res.json();
@@ -13,7 +22,7 @@ const fetchApi = async () => {
 }
 
 const getAllProducts = async (): Promise<number[]> => {
-  const products = await fetchApi()
+  const products = await fetchApi({ query: getAllProductQuery })
   return products.data
 }
 
